a115452b962c3e264c4f52c7451d6b5cdac03b34
Stephen Sykes <redhat4000@gmail.com>
2020-11-16 19:11:59 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a20201116g_versioncontrol/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a20201116g_versioncontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
